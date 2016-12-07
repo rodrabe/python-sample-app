98885a7597e8b1ad82cdf6d52a4750e36fbd002c
Brant Knudson <bknudson@us.ibm.com>
2016-12-07 10:54:07 -0600

    Change the output

--- testapp.py
+++ testapp.py
@@ -1,4 +1,4 @@
 
 def application(environ, start_response):
     start_response('200 OK', [('Content-Type', 'text/plain')])
-    return [b"Hello from gunicorn WSGI application!"]
+    return [b"Hello from Brant's gunicorn WSGI application!"]
