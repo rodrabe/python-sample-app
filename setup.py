72e97aadfb29b7dfe6bdbf7219f249f9cbe91bdd
Brant Knudson <bknudson@us.ibm.com>
2016-12-08 17:04:50 -0600

    Move mock requirement to test-requirements.txt

--- setup.py
+++ setup.py
@@ -5,5 +5,5 @@ setup(
     version="0.1",
     description="Example application to be deployed.",
     packages=find_packages(),
-    install_requires=['falcon', 'fixtures', 'gunicorn', 'mock', 'oslo.config'],
+    install_requires=['falcon', 'fixtures', 'gunicorn', 'oslo.config'],
 )
