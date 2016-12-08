6cbb00b31f36c58b83e2966188b2004558a05b4e
Brant Knudson <bknudson@us.ibm.com>
2016-12-08 17:10:39 -0600

    Require newer version of setuptools to test

--- setup.py
+++ setup.py
@@ -5,5 +5,11 @@ setup(
     version="0.1",
     description="Example application to be deployed.",
     packages=find_packages(),
-    install_requires=['falcon', 'fixtures', 'gunicorn', 'oslo.config'],
+    install_requires=[
+        'setuptools>=17.1',
+        'falcon',
+        'fixtures',
+        'gunicorn',
+        'oslo.config',
+    ],
 )
