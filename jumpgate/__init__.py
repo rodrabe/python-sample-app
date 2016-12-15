5ed48ea53940e4a092ceab7cf7ddd790d850c7de
Rod Rabe <rodrabe@us.ibm.com>
2016-12-15 15:49:13 -0600

    dddd

--- jumpgate/__init__.py
+++ jumpgate/__init__.py
@@ -1,46 +1,6 @@
-import logging
-import os
-import os.path
 
-from jumpgate import api
-from jumpgate import config as jumpgate_config
-
-import ConfigParser
-import json
-
-PROJECT = 'jumpgate'
-
-
-
-
-def make_api(config_path=None):
-    # Find configuration files
-    conf = ConfigParser.RawConfigParser()
-    config_files = conf.read([PROJECT])
-
-    # Check for environmental variable config file
-    env_config_loc = os.environ.get('JUMPGATE_CONFIG')
-    if env_config_loc and os.path.exists(env_config_loc):
-        config_files.insert(0, env_config_loc)
-
-    # Check for explit config file
-
-    if config_path and os.path.exists(config_path):
-        config_files.insert(0, config_path)
-
-    if not config_files:
-        raise Exception('No config files for %s found.' % PROJECT)
-
-    jumpgate_config.CONF(project=PROJECT,
-                         args=[],  # We don't want CLI arguments
-                         default_config_files=config_files)
-
-    logger = logging.getLogger(PROJECT)
-    logger.setLevel(getattr(logging,
-                            jumpgate_config.CONF['log_level'].upper()))
-    logger.addHandler(logging.StreamHandler())
-    app = api.Jumpgate()
-    app.load_endpoints()
-    app.load_drivers()
-
-    return app.make_api()
+__title__ = 'jumpgate'
+__version__ = 'v0.1'
+__author__ = 'SoftLayer Technologies, Inc.'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2013 SoftLayer Technologies, Inc.'
