b5280671fc4a9e5f4f2cbaf421b51b1dc37bc3a0
Rod Rabe <rodrabe@us.ibm.com>
2016-12-20 13:41:38 -0600

    dddd

--- jumpgate/common/hooks/__init__.py
+++ jumpgate/common/hooks/__init__.py
@@ -20,7 +20,7 @@ class APIHooks(object):
             self._loaded = False
 
         def load_hooks(self):
-            LOG.info("GOT HERE")
+            LOG.info("GOT HERE %s "% config.PARSER)
 
             if not self._loaded:
                 for hook in (['jumpgate.common.hooks.core'] +
