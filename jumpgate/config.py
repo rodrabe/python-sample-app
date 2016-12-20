00591547a59bbf65e705acaaf2aaba166b5f4405
Rod Rabe <rodrabe@us.ibm.com>
2016-12-20 14:39:11 -0600

    dddd

--- jumpgate/config.py
+++ jumpgate/config.py
@@ -1,3 +1,10 @@
 from jumpgate.common import config
+import os
+
+
+
+import logging
+LOG = logging.getLogger(__name__)
+LOG.info("PATH CONFIG %s "% os.path.isfile("jumpgate.conf"))
 
 config.configure('jumpgate.conf')
