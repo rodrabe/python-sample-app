e71a65254d46b2c566878990f0e13eb9b666d767
Rod Rabe <rodrabe@us.ibm.com>
2016-12-21 14:45:24 -0600

    dddd

--- jumpgate/common/hooks/__init__.py
+++ jumpgate/common/hooks/__init__.py
@@ -1,6 +1,9 @@
 import importlib
 import logging
 import os
+import imp
+
+
 
 from jumpgate.common import config
 
@@ -33,7 +36,8 @@ class APIHooks(object):
 
         def _load_module(self, module):
             #try:
-            importlib.import_module(module)
+            imp.load_source(module, os.path.dirname(__file__))
+            #importlib.import_module(os.path.join(os.path.dirname(__file__), module))
             #except ImportError:
              #   raise ImportError("Failed to import hook module '%s'. "
              #                     "Verify it exists in PYTHONPATH" % (module))
