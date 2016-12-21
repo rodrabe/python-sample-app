b03bb27a736522e049790c75a70acdf82cb84719
Rod Rabe <rodrabe@us.ibm.com>
2016-12-21 09:48:27 -0600

    dddd

--- jumpgate/wsgi.py
+++ jumpgate/wsgi.py
@@ -1,6 +1,8 @@
 import logging
 import os
 import os.path
+from jumpgate.common import config
+
 
 
 from jumpgate import api
@@ -30,6 +32,9 @@ def make_api(config_path=None):
     logger.info("PATH CONFIG AAAA %s "% virtualenv)
 
     logger.info("PATH CONFIG %s "% os.path.isfile(virtualenv))
+    config.PARSER.read(virtualenv)
+    a = config.PARSER.options('softlayer')
+    logger.info("Options %s"% a)
     app = api.Jumpgate()
     app.load_endpoints()
     app.load_drivers()
