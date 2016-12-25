ac689565d95012d14912d36caae94f8c1d3d42b0
Rod Rabe <rodrabe@us.ibm.com>
2016-12-25 16:42:01 -0600

    dddd

--- jumpgate/config.py
+++ jumpgate/config.py
@@ -2,9 +2,17 @@ from jumpgate.common import config
 import os
 
 
-
+PROJECT = 'jumpgate'
 import logging
-LOG = logging.getLogger(__name__)
-LOG.info("PATH CONFIG %s "% os.path.isfile("jumpgate.conf"))
+logger = logging.getLogger(PROJECT)
+logger.setLevel('INFO')
+logger.addHandler(logging.StreamHandler())
+logger.info("PATH CONFIG  AAA%s "% os.getcwd())
+virtualenv = os.path.join(os.path.dirname(__file__), 'jumpgate.conf')
 
-config.configure('jumpgate.conf')
+logger.info("PATH CONFIG AAAA %s "% virtualenv)
+
+logger.info("PATH CONFIG %s "% os.path.isfile(virtualenv))
+config.PARSER.read(virtualenv)
+a = config.PARSER.options('softlayer')
+logger.info("Options %s"% a)
