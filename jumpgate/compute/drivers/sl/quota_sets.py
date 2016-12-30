1be24829b1d85cc9fe2741f42145ef51227353b3
Rod Rabe <rodrabe@us.ibm.com>
2016-12-29 18:42:36 -0600

    dddd

--- jumpgate/compute/drivers/sl/quota_sets.py
+++ jumpgate/compute/drivers/sl/quota_sets.py
@@ -1,24 +1,24 @@
-from oslo_config import cfg
+from jumpgate.common import config
 
 
 class OSQuotaSetsV2(object):
     def on_get(self, req, resp, tenant_id, account_id=None):
         qs = {
-            "cores": cfg.CONF['compute']['default_cores'],
-            "floating_ips": cfg.CONF['compute']['default_floating_ips'],
+            "cores": config.PARSER.get("compute",'default_cores'),
+            "floating_ips": config.PARSER.get("compute",'default_floating_ips'),
             "id": tenant_id,
             "injected_file_content_bytes":
-            cfg.CONF['compute']['default_injected_file_content_bytes'],
+            config.PARSER.get("compute",'default_injected_file_content_bytes'),
             "injected_file_path_bytes":
-            cfg.CONF['compute']['default_injected_file_path_bytes'],
-            "injected_files": cfg.CONF['compute']['default_injected_files'],
-            "instances": cfg.CONF['compute']['default_instances'],
-            "key_pairs": cfg.CONF['compute']['default_key_pairs'],
-            "metadata_items": cfg.CONF['compute']['default_metadata_items'],
-            "ram": cfg.CONF['compute']['default_ram'],
+            config.PARSER.get("compute",'default_injected_file_path_bytes'),
+            "injected_files": config.PARSER.get("compute",'default_injected_files'),
+            "instances": config.PARSER.get("compute",'default_instances'),
+            "key_pairs": config.PARSER.get("compute",'default_key_pairs'),
+            "metadata_items": config.PARSER.get("compute",'default_metadata_items'),
+            "ram": config.PARSER.get("compute",'default_ram'),
             "security_group_rules":
-            cfg.CONF['compute']['default_security_group_rules'],
-            "security_groups": cfg.CONF['compute']['default_security_groups']
+            config.PARSER.get("compute",'default_security_group_rules'),
+            "security_groups": config.PARSER.get("compute",'default_security_groups')
         }
 
         resp.body = {'quota_set': qs}
